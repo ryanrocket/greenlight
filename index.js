0554f989dbbdc4ee946d4ab3724986f10d134a80
Ryan <ryan@ryanwans.com>
2020-11-26 12:26:49 -0500

    Automated Push

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 // Greenlight (C) 2020 by Ryan Wans
 
+const { createHmac } = require('crypto');
+
 -(function() {
-    
+    var fs = require('fs'),
+        { exec } = require('child_process'),
+        DELAY = 600000;
+
+    var EXEC_CHMOD = exec('chmod +x _c_pull.sh',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log("GRNLT Err [DEBUG] - The following error is most likely because of permission errors when attempting to execute a needed shell script. Please try again with sudo");
+                console.log(`GRNLT Err: ${error}`);
+            }
+        });
+
+    setInterval(() => {
+        var now = new Date();
+        try {
+            var EXEC_PULL = exec('sh _c_pull.sh',
+                (error, stdout, stderr) => {
+                    console.log(stdout);
+                    console.log(stderr);
+                    if (error !== null) {
+                        console.log(`GRNLT Err: ${error}`);
+                    }
+                });
+        } catch (e) {
+            try {
+                exec.execFile('_c_pull.sh');
+                console.log("GRNLT Err [DEBUG] - Error running normal script. Running backup version");
+            } catch(e) {
+                throw new Error(e);
+            }
+        }
+    }, DELAY);
 })();
