8a639998a20e922824ec18a2ded0e1989c682c95
Ryan <ryan@ryanwans.com>
2020-11-26 13:30:39 -0500

    Automated Push

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-// Greenlight (C) 2020 by Ryan Wans
-
-const { createHmac } = require('crypto');
+// Greenlight (c) Ryan Wans 2020
 
 -(function() {
     var fs = require('fs'),
         { exec } = require('child_process'),
-        DELAY = 600000;
+        cron = require('node-cron'),
+        DELAY = 600000,
+        that = {hasOpts: false};
 
+    // Ensure pull file is executable
     var EXEC_CHMOD = exec('chmod +x _c_pull.sh',
         (error, stdout, stderr) => {
             console.log(stdout);
@@ -16,7 +17,47 @@ const { createHmac } = require('crypto');
                 console.log(`GRNLT Err: ${error}`);
             }
         });
+    
+    // Check for server update file
+    try {
+        var fileBuffer = fs.readFileSync('../greenlight.json', {root: __dirname});
+        that.opts = JSON.parse(fileBuffer);
+        that.hasOpts = true;
+    } catch(e) {
+        try {
+            var fileBuffer = fs.readFileSync('./greenlight.json', {root: __dirname});
+            that.opts = JSON.parse(fileBuffer);
+            that.hasOpts = true;
+        } catch(e) {
+            that.hasOpts = false;
+            console.log("GRNLT Err [DEBUG] - Could not locate a 'greenlight.json' config file, meaning your server won't restart. Persisting execution.");
+        }
+    }
 
+    // If they setup a server config, create the new cron job
+    if(Object.keys(that.opts).includes("cron")) {
+        if("object" != typeof that.opts.update) {
+            console.log("GRNLT Err [DEBUG] - In the config json, key 'update' should be an array of strings containing commands to execute in order.");
+        } else {
+            console.log("GRNLT DEBUG - Successfully located cron, scheduled update!");
+            cron.schedule(that.opts.cron, () => {
+                for(let i=0; i<that.opts.update.length; i++) {
+                    var cmd = that.opts.update[i];
+                    try {
+                        var _tmp_execution = exec(cmd, (e,o,se) => {
+                            if(e !== null) {`GRNLT Err: ${error}`}
+                        })
+                    } catch(e) {
+                        throw new Error("GRNLT - Error upon executing an update command at index: "+i);
+                    }
+                }
+            })
+        }
+    } else {
+        console.log("GRNLT Err [DEBUG] - Could not location the cron code inside of the config json file. Use key 'cron'.");
+    }
+    
+    // Start polling the pull file
     setInterval(() => {
         var now = new Date();
         try {
--- run.m.js
+++
@@ -1,5 +0,0 @@
-// Greenlight (C) 2020 by Ryan Wans
-
-!(function() {
-    
-})()
