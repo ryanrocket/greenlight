95fe83e54541629ea0f8604bb62a976b2c583782
Ryan <ryan@ryanwans.com>
2020-11-26 11:55:14 -0500

    GREENLIGHT: Automated Push

--- grnlght/_c_pull.sh
+++ grnlght/_c_pull.sh
@@ -14,10 +14,15 @@ if [ -d .git ]; then
     if git diff-index --quiet HEAD --; then
         echo "This repo is already on the latest working commit."
     else
-        echo "Current version is outdated, rebasing..."
-
+        echo "Current version is outdated! Pushing changes..."
+        git add -A 
+        git commit -m "GREENLIGHT: Automated Push"
+        echo "Stashed. Pulling latest version..."
+        git pull --rebase 
+        git push origin master
     fi
 else 
     echo "This does not appear to be a git repo."
     git rev-parse --git-dir 2> /dev/null;
 fi;
+echo "script finished."
